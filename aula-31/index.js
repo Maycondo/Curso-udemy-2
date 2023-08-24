0eae65edc06dc79f3cc47933c4399bff4d0bb795
Maycon <maycondouglasniculau@gmail.com>
2023-08-24 19:01:10 -0300

    Curso-2

---
+++ aula-31/index.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Michael",
+  surname: "douglas",
+  idade: "20 anos de idades",
+  endereço: {
+    Estado: "Alagoas/Maceio",
+    Cidade: "Rio Largo",
+    Rua: "R.Quinze vila rica",
+  },
+};
+
+console.log(person.name)
