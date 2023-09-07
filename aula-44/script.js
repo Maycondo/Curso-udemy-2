81bf0c8ecefd8430db8fc6e573ebdd80fb8d22d6
Maycon <maycondouglasniculau@gmail.com>
2023-09-07 16:15:50 -0300

    Curso-2

--- aula-44/script.js
+++ aula-44/script.js
@@ -1,33 +1,48 @@
-function criaHoraDosSegundos(segundos){
-    const data = new Date(segundos)
-    return data.toLocaleTimeString('pt-br',{
-        hour12: false,
-        timeZone: 'UTM'
+function Relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "UTC",
     });
+  }
+
+  const relogio = document.querySelector(".relogio");
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener("click", function (e) {
+    const el = e.target;
+
+    if (el.classList.contains("zerar")) {
+      clearInterval(timer);
+      relogio.innerHTML = "00:00:00";
+      segundos = 0;
+    }
+
+    if (el.classList.contains("iniciar")) {
+      relogio.classList.remove("pausado");
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (el.classList.contains("pausar")) {
+      relogio.classList.add("pausado");
+      clearInterval(timer);
+    }
+  });
 }
-
-
-const relogio = document.querySelector('.Relogio')
-const iniciar = document.querySelector('.iniciar')
-const pausar = document.querySelector('.pausar')
-const zerar = document.querySelector('.zerar')
-let segundos = 0;
-
-function iniciaRelogio() {
-    const timer = setInterval(function(){
-        segundos++;
-        relogio
-    },1000)
-}
-
-iniciar.addEventListener('click', function(evento){
-    relogio.innerHTML = 'clliquei no iniciar';
-})
-
-pausar.addEventListener('click', function(evento){
-    alert('clliquei no iniciar');
-})
-
-zerar.addEventListener('click', function(evento){
-    alert('clliquei no iniciar');
-})
